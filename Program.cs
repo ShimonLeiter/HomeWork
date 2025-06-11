ef792065b8958183e428fe1853e55f447d7943a2
ShimonLeiter <s.leiter306@gmail.com>
2025-06-11 14:50:29 -0400

    Here is the C# homework

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HW49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] color = { "red", "green", "blue" };
+            string[] patterns = { "stripped", "checked", "plain" };
+
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                for (int k = 0; k < patterns.Length; k++)
+                {
+
+                    Shirt shirt = new Shirt(color[i], patterns[k]);
+
+                }
+            }
+
+
+
+        }
+    }
+}

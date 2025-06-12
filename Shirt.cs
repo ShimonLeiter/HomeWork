7abf3bfdbeb8897628e4016d1841ae45cefa1c1b
ShimonLeiter <s.leiter306@gmail.com>
2025-06-12 19:00:58 -0400

    testing

---
+++ Shirt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW49
+{
+    internal class Shirt
+    {
+       public string color {  get; set; }   
+
+        public string pattern { get; set; }
+
+        public Shirt(string color, string pattern)
+        {
+            this.color = color;
+            this.pattern = pattern;
+            Console.WriteLine($"{this.color}:{this.pattern} ");
+
+        }
+    }
+}
